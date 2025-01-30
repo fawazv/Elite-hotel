ad306a3ea33357bf1a753aa5769a43a1c9970b19
Fawaz v <fawazchemmad@gmail.com>
2025-01-30 15:05:01 +0530

    created user interface for user modal'

---
+++ backend/services/authService/src/interfaces/IUser.ts
@@ -0,0 +1,16 @@
+import { Document, ObjectId } from "mongoose";
+
+interface IUser extends Document {
+  _id: ObjectId;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  username: string;
+  password: string;
+  role: "receptionist" | "housekeeper" | "admin";
+  isApproved: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export default IUser;
