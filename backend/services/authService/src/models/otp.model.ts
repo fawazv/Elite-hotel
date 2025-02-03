8eaf30040e17a39bb4ddf11956704dbcac20eb24
Fawaz v <fawazchemmad@gmail.com>
2025-02-03 15:47:00 +0530

    created otp model

---
+++ backend/services/authService/src/models/otp.model.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+import IOtp from "../interfaces/IOtp";
+
+const otpSchema: Schema = new Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: "60s" },
+});
+
+const Otp = mongoose.model<IOtp>("Otp", otpSchema);
+export default Otp;
