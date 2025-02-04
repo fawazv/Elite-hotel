9cbaa1a7296cd29e1583fdfa5679fda2ba40de55
Fawaz v <fawazchemmad@gmail.com>
2025-02-04 08:15:45 +0530

    created baserepository and its interface

---
+++ backend/services/authService/src/repository/interface/IBase.repository.ts
@@ -0,0 +1,5 @@
+export interface IBaseRepository<T> {
+  create(data: Partial<T>): Promise<T>;
+  findById(id: string): Promise<T | null>;
+  update(id: string, data: Partial<T>): Promise<T | null>;
+}
