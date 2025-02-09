84c18e4644cba25734644d5bd8ac30966ae7a239
Fawaz v <fawazchemmad@gmail.com>
2025-02-09 16:14:21 +0530

    applied the error handler in index.ts

--- backend/services/authService/src/index.ts
+++ backend/services/authService/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
+
+// Global error handling middleware
+app.use(errorHandler);
 
 app.listen(3001, () => console.log(`server running on http://localhost:3001`));
