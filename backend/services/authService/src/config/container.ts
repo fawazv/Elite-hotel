2a482ceb98536c677d16a7c01b282e3cc652cfc2
Fawaz v <fawazchemmad@gmail.com>
2025-02-13 10:44:20 +0530

    created dependency injection

---
+++ backend/services/authService/src/config/container.ts
@@ -0,0 +1,20 @@
+import { AuthController } from "../controllers/implementation/auth.controller";
+import Otp from "../models/otp.model";
+import { User } from "../models/user.model";
+import { OtpRepository } from "../repository/implementation/otp.repository";
+import { UserRepository } from "../repository/implementation/user.respository";
+import { AuthService } from "../services/implementation/auth.service";
+
+// DEPENDENCY INJECTION
+
+// repositories
+const userRepository = new UserRepository(User);
+const otpRepository = new OtpRepository(Otp);
+
+// Services
+const authService = new AuthService(userRepository, otpRepository);
+console.log("AuthService initialized:", !!authService);
+
+const authController = new AuthController(authService);
+
+export { authController, authService };
