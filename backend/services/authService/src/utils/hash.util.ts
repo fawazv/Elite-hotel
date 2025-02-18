3c229e2af66b6674465e58771d0c091e927cc704
Fawaz v <fawazchemmad@gmail.com>
2025-02-18 16:23:46 +0530

    created hash util to hash passwords

---
+++ backend/services/authService/src/utils/hash.util.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcryptjs";
+
+export const randomPassword = bcrypt.hashSync(
+  Math.random().toString(36).slice(-8),
+  10
+);
+
+export const hashPassword = async (password: string) => {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
+};
