46ed3518492abc44af0344d7e67036b37cb59c8f
Fawaz v <fawazchemmad@gmail.com>
2025-03-21 15:56:05 +0530

    creeated auth slice for authentication

---
+++ frontend/src/redux/slices/authSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  id: string;
+  fullName?: string;
+  email: string;
+  role: "admin" | "receptionist" | "housekeeper";
+}
+
+interface AuthState {
+  token: string | null;
+  isAuthenticated: boolean;
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  token: null,
+  isAuthenticated: false,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<{ token: string; user: User }>) => {
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.isAuthenticated = false;
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
