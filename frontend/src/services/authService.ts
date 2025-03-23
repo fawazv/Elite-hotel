ab169efc9c8689381f1d5fb01648bebf07e4e29f
Fawaz v <fawazchemmad@gmail.com>
2025-03-23 15:58:53 +0530

    created authService and updated path of axiosConfig.ts

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,84 @@
+import { api, privateApi } from "@/services/instances/axiosConfig";
+
+export const signInRequest = async (
+  email: string,
+  password: string,
+  role: string
+) => {
+  const response = await api.post("/signin", { email, password, role });
+  return response.data;
+};
+
+export const signUpRequest = async (email: string) => {
+  const response = await api.post("/signup", { email });
+  return response.data;
+};
+
+export const otpVerify = async (
+  fullName: string,
+  email: string,
+  password: string,
+  role: string,
+  phoneNumber: string,
+  otp: string,
+  type: string
+) => {
+  const response = await api.post("/otp-signup", {
+    fullName,
+    email,
+    password,
+    role,
+    phoneNumber,
+    otp,
+    type,
+  });
+  return response;
+};
+
+export const resendOtp = async (email: string) => {
+  const response = await api.post("/otp-resend", { email });
+  return response;
+};
+
+export const googleSignIn = async (
+  email: string,
+  name: string,
+  role: string,
+  phoneNumber: string
+) => {
+  const response = await api.post("/google-signin", {
+    email,
+    name,
+    role,
+    phoneNumber,
+  });
+  return response.data;
+};
+
+export const sendMail = async (email: string) => {
+  const response = await api.post("/forget-password", { email });
+  return response.data;
+};
+
+export const resetPassword = async (
+  email: string,
+  password: string,
+  confirmPassword: string
+) => {
+  const response = await api.post("/reset-password", {
+    email,
+    password,
+    confirmPassword,
+  });
+  return response.data;
+};
+
+export const logout = async () => {
+  const response = await api.post("/logout");
+  return response.data;
+};
+
+export const passwordUpdate = async (data: object) => {
+  const response = await privateApi.patch("/user/change-password", data);
+  return response.data;
+};
