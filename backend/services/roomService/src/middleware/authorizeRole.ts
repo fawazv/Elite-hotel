dba208a2c51c8d3dee9ef8efb80706cb72fe4725
Fawaz v <fawazchemmad@gmail.com>
2025-08-15 12:34:51 +0530

    add custom request

--- backend/services/roomService/src/middleware/authorizeRole.ts
+++ backend/services/roomService/src/middleware/authorizeRole.ts
@@ -1,9 +1,10 @@
-import { Request, Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 import { HttpStatus } from '../enums/http.status'
 import CustomError from '../utils/CustomError'
+import { CustomeRequest } from '../interfaces/CustomRequest'
 
 export const authorizeRole = (roles: string[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: CustomeRequest, res: Response, next: NextFunction) => {
     const userRole = req.user?.role
     if (!userRole || !roles.includes(userRole)) {
       throw new CustomError(
