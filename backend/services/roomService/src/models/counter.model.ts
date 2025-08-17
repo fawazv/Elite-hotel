b5ae901f222594f782fe943f09d16b5f2f2c5c8c
Fawaz v <fawazchemmad@gmail.com>
2025-08-17 18:31:37 +0530

    intitialized reservation service

--- backend/services/roomService/src/models/counter.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const CounterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 },
-})
-
-export const CounterModel = mongoose.model('Counter', CounterSchema)
