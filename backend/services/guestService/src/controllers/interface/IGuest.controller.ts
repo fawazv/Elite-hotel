b28d6a80c05416b603ebce311a95aeaa7ad9daef
Fawaz v <fawazchemmad@gmail.com>
2025-08-23 14:05:06 +0530

    create guest controller

---
+++ backend/services/guestService/src/controllers/interface/IGuest.controller.ts
@@ -0,0 +1,28 @@
+// src/controllers/interface/IGuest.controller.ts
+import { Request, Response, NextFunction } from 'express'
+
+export interface IGuestController {
+  create(req: Request, res: Response, next: NextFunction): Promise<void>
+  getById(req: Request, res: Response, next: NextFunction): Promise<void>
+  list(req: Request, res: Response, next: NextFunction): Promise<void>
+  update(req: Request, res: Response, next: NextFunction): Promise<void>
+  patch(req: Request, res: Response, next: NextFunction): Promise<void>
+  remove(req: Request, res: Response, next: NextFunction): Promise<void>
+
+  updateIdProofImage(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void>
+  removeIdProofImage(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void>
+
+  ensureForBooking(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void>
+}
