396f33a7283c3205de76595686a0213d7e3bd210
Fawaz v <fawazchemmad@gmail.com>
2025-09-09 16:27:00 +0530

    create di wiring

---
+++ backend/services/paymentService/src/config/container.ts
@@ -0,0 +1,8 @@
+// src/config/container.ts
+import { PaymentRepository } from '../repository/implementation/payment.repository'
+import { PaymentService } from '../services/implementation/payment.service'
+import { PaymentController } from '../controllers/payment.controller'
+
+const paymentRepository = new PaymentRepository()
+const paymentService = new PaymentService(paymentRepository)
+export const paymentController = new PaymentController(paymentService)
