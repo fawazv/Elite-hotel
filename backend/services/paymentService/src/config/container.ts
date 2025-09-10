371d43e8d59f1c6580b28ce98a286eaff2d607c6
Fawaz v <fawazchemmad@gmail.com>
2025-09-10 09:12:06 +0530

    create webhook. routes and update container(di)

--- backend/services/paymentService/src/config/container.ts
+++ backend/services/paymentService/src/config/container.ts
@@ -2,7 +2,9 @@
 import { PaymentRepository } from '../repository/implementation/payment.repository'
 import { PaymentService } from '../services/implementation/payment.service'
 import { PaymentController } from '../controllers/implementation/payment.controller'
+import { WebhookController } from '../controllers/implementation/webhook.controller'
 
 const paymentRepository = new PaymentRepository()
 const paymentService = new PaymentService(paymentRepository)
 export const paymentController = new PaymentController(paymentService)
+export const webhookController = new WebhookController(paymentService)
