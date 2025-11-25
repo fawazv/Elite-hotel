b894d21d6142fe3680cb34533b5e6980428ac396
Fawaz v <fawazchemmad@gmail.com>
2025-11-25 14:16:53 +0530

    feat: add guest controller interface with methods for guest management and ID proof image handling

--- backend/services/guestService/src/controllers/interface/IGuest.controller.ts
+++ backend/services/guestService/src/controllers/interface/IGuest.controller.ts
@@ -2,26 +2,26 @@
 import { Request, Response, NextFunction } from 'express'
 
 export interface IGuestController {
-  create(req: Request, res: Response, next: NextFunction): Promise<void>
-  getById(req: Request, res: Response, next: NextFunction): Promise<void>
-  list(req: Request, res: Response, next: NextFunction): Promise<void>
-  patch(req: Request, res: Response, next: NextFunction): Promise<void>
-  remove(req: Request, res: Response, next: NextFunction): Promise<void>
+  create(req: Request, res: Response, next: NextFunction): Promise<any>
+  getById(req: Request, res: Response, next: NextFunction): Promise<any>
+  list(req: Request, res: Response, next: NextFunction): Promise<any>
+  patch(req: Request, res: Response, next: NextFunction): Promise<any>
+  remove(req: Request, res: Response, next: NextFunction): Promise<any>
 
   updateIdProofImage(
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void>
+  ): Promise<any>
   removeIdProofImage(
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void>
+  ): Promise<any>
 
   ensureForBooking(
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void>
+  ): Promise<any>
 }
